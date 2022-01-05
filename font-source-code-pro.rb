81e1f824ba2a8d9b5a5f64641c6374ec6bdac3d6
Jim Wu <jim.wu@clorox.com>
2022-01-05 03:30:23 -0800

    Update url and font paths

--- font-source-code-pro.rb
+++ font-source-code-pro.rb
@@ -2,12 +2,10 @@ cask "font-source-code-pro" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/sourcecodepro",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://fonts.google.com/download?family=Source+Code+Pro"
   name "Source Code Pro"
   homepage "https://fonts.google.com/specimen/Source+Code+Pro"
 
-  font "SourceCodePro-Italic[wght].ttf"
-  font "SourceCodePro[wght].ttf"
+  font "SourceCodePro-VariableFont_wght.ttf"
+  font "SourceCodePro-Italic-VariableFont_wght.ttf"
 end
